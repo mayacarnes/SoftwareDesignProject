3511da4838f86cd8b727c4058aec0844f7f8ad6c
mayacarnes <maya.carnes@trincoll.edu>
2024-10-17 23:31:39 -0400

    Switch JVM toolchain to 17
    This change updates the Kotlin JVM toolchain from version 21 to 17. It ensures compatibility with environments currently using JDK 17, potentially improving stability and reducing runtime issues.

--- src/main/kotlin/edu/trincoll/hr/Employee.kt
+++ src/main/kotlin/edu/trincoll/hr/Employee.kt
@@ -1,5 +1,7 @@
 package edu.trincoll.hr
 
+import edu.trincoll.shapes.Shape
+
 // Abstract class Employee should have:
 //   - a name of type String
 //   - an id of type Int.
@@ -10,7 +12,21 @@ package edu.trincoll.hr
 // that returns a Double.
 //
 // It should override the toString method to
-// return a string with the name and id of the employee.
-abstract class Employee(
-) {
+// return a string!!!! with the name and id of the employee.
+
+
+abstract class Employee(val name: String, val id: Int) : Comparable<Employee> {
+
+   abstract fun pay(): Double
+
+    override fun compareTo(other: Employee): Int {
+        return this.id.compareTo(other.id)
+    }
+
+
+   override fun toString(): String {
+       return "Employee Name: $name, Employee ID: $id"
+   }
+
 }
+
--- src/main/kotlin/edu/trincoll/hr/HR.kt
+++ src/main/kotlin/edu/trincoll/hr/HR.kt
@@ -1,10 +1,35 @@
 package edu.trincoll.hr
 
+import java.util.Objects
+
 // The HR class should have:
 //   - a list of employees
 //   - a hire method that takes an employee and returns a new HR object with that employee added
 //   - a fire method that takes an id and returns a new HR object with the employee with that id removed
 //   - a payEmployees method that returns the total pay of all employees
+
+
 class HR(private val employees: List<Employee> = emptyList()) {
 
+    fun hire(employee: Employee): HR{
+        val newEmployees = employees + employee
+        return HR(newEmployees)
+    }
+
+   fun fire(employeeId: Int): HR {
+
+       val newEmployees2 = employees.filter { it.id != employeeId}
+       return HR(newEmployees2)
+   }
+
+
+    fun payEmployees(): Double {
+        var totalPay = 0.0
+        for (employee in employees) {
+            totalPay = employee.pay() + totalPay
+        }
+
+        return totalPay
+
+    }
 }
